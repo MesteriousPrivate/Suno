00ebdc46f86db37ee68968ca574c883722e3adbd
Nand Yaduwanshi <fmdevil2@gmail.com>
2025-05-19 23:48:21 +0530

    ShrutiBots

--- ShrutiMusic/plugins/bot/start.py
+++ ShrutiMusic/plugins/bot/start.py
@@ -84,19 +84,19 @@ async def start_pm(client, message: Message, _):
                     text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>ᴛʀᴀᴄᴋ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</b>.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
                 )
     else:
-    out = private_panel(_)
-    UP, CPU, RAM, DISK = await bot_sys_stats()
-    await message.reply_photo(
-        photo=config.START_IMG_URL,
-        caption=_["start_2"].format(message.from_user.mention, app.mention, UP, DISK, CPU, RAM),
-        reply_markup=InlineKeyboardMarkup(out),
-        message_effect_id=5104841245755180586  # Effect set here 🔥
-    )
-    if await is_on_off(2):
-        return await app.send_message(
-            chat_id=config.LOG_GROUP_ID,
-            text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
+        out = private_panel(_)
+        UP, CPU, RAM, DISK = await bot_sys_stats()
+        await message.reply_photo(
+            photo=config.START_IMG_URL,
+            caption=_["start_2"].format(message.from_user.mention, app.mention, UP, DISK, CPU, RAM),
+            reply_markup=InlineKeyboardMarkup(out),
+            message_effect_id=5104841245755180586  # <-- Effect added here
         )
+        if await is_on_off(2):
+            return await app.send_message(
+                chat_id=config.LOG_GROUP_ID,
+                text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
+            )
 
 
 @app.on_message(filters.command(["start"]) & filters.group & ~BANNED_USERS)
